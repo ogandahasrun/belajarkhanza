599b06e81acac07303ad2f043ff0d3987c2986c6
ogandahasrun <112628704+ogandahasrun@users.noreply.github.com>
2024-04-29 17:01:43 +0700

    Add files via upload

---
+++ rsudpringsewu/koneksi.php
@@ -0,0 +1,12 @@
+<?php 
+$koneksi = mysqli_connect("103.151.140.164","backup","backup","sik");
+//"localhost","root","","sik"
+//"103.151.140.164","backup","backup","sik"
+ 
+// Check connection
+if (mysqli_connect_errno()){
+	echo "Koneksi database gagal : " . mysqli_connect_error();
+}else{
+echo "";
+}
+?>
